6c662f4de82a7869bf30eb9e7d042dcd5eb17006
Tyler <tyler.jankowski@hotmail.com>
2022-06-08 13:21:46 -0400

    added download resume end point

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, send_file
 import csv
 
 app = Flask(__name__)
 print(__name__)
+
+DOWNLOAD_FOLDER = 'downloads/'
 
 
 @app.route('/')
@@ -18,32 +20,37 @@ def router(route=None):
         return render_template(route + '.html')
 
 
-@app.route('/submit_form', methods=['POST', 'GET'])
-def submit_form():
-    if request.method == 'POST':
-        try:
-            data = request.form.to_dict()
-            write_to_csv(data)
-            return redirect('thankyou.html')
-        except:
-            return 'did not save to database'
-    else:
-        return 'something went wrong'
+# @app.route('/submit_form', methods=['POST', 'GET'])
+# def submit_form():
+#     if request.method == 'POST':
+#         try:
+#             data = request.form.to_dict()
+#             write_to_csv(data)
+#             return redirect('thankyou.html')
+#         except:
+#             return 'did not save to database'
+#     else:
+#         return 'something went wrong'
 
+# return downloadable file 
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = DOWNLOAD_FOLDER + filename
+    return send_file(file_path, as_attachment=True, attachment_filename='')
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        file = database.write(f'\n{email},{subject},{message}')
-        database.close()
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f'\n{email},{subject},{message}')
+#         database.close()
 
-def write_to_csv(data):
-    with open('database.csv', newline='', mode='a') as database2:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email, subject, message])
-    database2.close()
+# def write_to_csv(data):
+#     with open('database.csv', newline='', mode='a') as database2:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#         csv_writer.writerow([email, subject, message])
+#     database2.close()
