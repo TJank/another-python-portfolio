66bf6ed818414a624bc747ec3dabb74ba2d94b02
Tyler <tyler.jankowski@hotmail.com>
2022-06-08 22:27:02 -0400

    added 404 page updated email layout with html

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect, send_file
 import csv
+import yagmail
 
 app = Flask(__name__)
-print(__name__)
 
 DOWNLOAD_FOLDER = 'downloads/'
 
@@ -14,23 +14,40 @@ def my_home():
 
 @app.route('/<string:route>')
 def router(route=None):
-    if '.html' in route:
-        return render_template(route)
+    try:
+        if '.html' in route:
+            return render_template(route)
+        else:
+            return render_template(route + '.html')
+    except Exception as e:
+        return render_template('404.html', title = '404'), 404
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            print("got inside try catch")
+            email = yagmail.SMTP(user="auto.email.tj@gmail.com", password="vfmvpukkglkgioaj")
+            data = request.form.to_dict()
+            user_email = data['email']
+            user_subject = data['subject']
+            message = data['message']
+            print("data looks good so far")
+            email.send(to="jankowski_tj@outlook.com",
+            subject=f"PORTFOLIO WEBSITE: {user_subject}",
+            contents=f"""<h2>{user_subject}</h2>
+                        <h3>You have received the following message from <b>{user_email}</b></h4>
+                        <h4>Message:</h4>
+                        <p>{message}</p>""")
+            print("problem on send")
+            return redirect('thankyou.html')
+        except Exception as e:
+            print(e)
+            return 'something went wrong....'
     else:
-        return render_template(route + '.html')
+        return 'something went wrong'
 
-
-# @app.route('/submit_form', methods=['POST', 'GET'])
-# def submit_form():
-#     if request.method == 'POST':
-#         try:
-#             data = request.form.to_dict()
-#             write_to_csv(data)
-#             return redirect('thankyou.html')
-#         except:
-#             return 'did not save to database'
-#     else:
-#         return 'something went wrong'
 
 # return downloadable file 
 @app.route('/return-files/<filename>')
@@ -38,19 +55,3 @@ def return_files_tut(filename):
     file_path = DOWNLOAD_FOLDER + filename
     return send_file(file_path, as_attachment=True, attachment_filename='')
 
-# def write_to_file(data):
-#     with open('database.txt', mode='a') as database:
-#         email = data['email']
-#         subject = data['subject']
-#         message = data['message']
-#         file = database.write(f'\n{email},{subject},{message}')
-#         database.close()
-
-# def write_to_csv(data):
-#     with open('database.csv', newline='', mode='a') as database2:
-#         email = data['email']
-#         subject = data['subject']
-#         message = data['message']
-#         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#         csv_writer.writerow([email, subject, message])
-#     database2.close()
